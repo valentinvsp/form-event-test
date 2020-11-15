0deb69fa6c1912501dfd506e63491d85ca51d8f6
Valentin Harapu <valentin.harapu@vspartners.us>
2020-11-15 17:58:47 +0200

    add press Enter to focus next or submit

--- src/components/SuperForm/SuperForm.js
+++ src/components/SuperForm/SuperForm.js
@@ -1,82 +1,87 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef, useEffect, createRef } from 'react';
 import './SuperForm.scss';
 
 /*
 TODO
-1. Initialize form state on mount with info derived from input elements
-2. Pass refs into the state?
-3. Use the refs to add eveents that send your focus to the next input element when pressing enter?
-OR catch the event on the form element and use the array to focus the next target in its array?
-4. 
+1. Initialize form state on mount with info derived from input elements (DONE)
+2. Pass refs of each input's next input element into it's state. (DONE)
+3. Use the refs to add an event that send your focus to the next input element when pressing enter. (DONE)
+4. Add an onBlur event to turn touched to true AND validate that element. 
+5. Add a validation event for each keypress or click on an input. The validation should only trigger if touched is true.
+6. Add form level validation for the handleSubmit function.
 */
+
+// TODO add event listeners to the form (using event delegation) to alter state when necessary, validate on blur etc.
+
+// use these attributes for validation 
+// console.log('valid: ', e.target[0].validity.valid); 
+// console.log('value missing: ', e.target[0].validity.valueMissing);
+
+const ENTER = 13;
 
 const SuperForm = () => {
     const [formData, setFormData] = useState({})
     const signupForm = useRef(null);
+    const renderCount = useRef(0);
 
     const createStateObjFromForm = (form) => {
         let state = {};
-        console.log(form.elements);
-        const inputArray = new Array(...form.elements);
-        console.log(inputArray);
-        inputArray.forEach( input => {
+        new Array(...form.elements).forEach( ( input, idx, arr )=> {
             if (input.type === 'submit') return;
             const inputState = {
+                index: idx,
                 name: input.id,
                 value: input.value,
                 touched: false,
-                error: input.dataset.error
+                error: input.dataset.error,
+                nextInput: arr[idx+1],
             }
             state = {...state, [input.id]: inputState };
-        })
-        console.log('state at end of create state function: \n', state);
+        });
         return state;
     }
+
+    const handleKeyDown = e => {
+        if(e.keyCode == 13) e.preventDefault();
+    }
+
+    const handleKeyUp = e => {
+        e.preventDefault();
+        if (e.keyCode === ENTER && e.target.id ) return formData[e.target.id].nextInput.focus();
+        if (e.target.type === 'submit') return handleSubmit(); 
+        const formState = {...formData};
+        formState[e.target.id].value = e.target.value;
+        setFormData(formState);        
+    }
     
-    // use the form ref to cycle through form elements and initialize state
-    // and add event listeners to the form (using event delegation) to alter state when necessary, validate on blur etc.
     useEffect(() => {
-        if (signupForm.current) setFormData(createStateObjFromForm(signupForm.current));
+        if (signupForm.current) {
+            const state = createStateObjFromForm(signupForm.current);
+            setFormData(state);
+        }
+
         return () => {
             
         }
     }, [])
 
     const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log('SUBMITED')
-
-        // use these event attributes to create initial state when cycling through elements at mount
-        // OR use the form ref to do it
-        console.log(e.target[0].id); 
-        console.log(e.target[0].validationMessage);
-
-        // use these attributes for validation 
-        console.log('valid: ', e.target[0].validity.valid); 
-        console.log('value missing: ', e.target[0].validity.valueMissing);
-
-        e.target[0].addEventListener('click', () => console.log('clicked'));  // this method of adding event listeners works.
-        // use this on mount to cycle through inputs and add "kepress enter" and "blur" event listeners
-        // OR don't, and use event delegation instead
-
-
-        // console.log(e.target[0].);
-        // e.target.target[0].setCustomValidity('i am the custom message');
-        // e.target.elements.user.validationMessage = 'maybe this works';
-        
-        console.log(e);
-        console.log('current form state: ', formData);
+        if (e) e.preventDefault();
+        console.log('SUBMITTED');
     }
     
+    renderCount.current++;
+    console.log(`render number ${renderCount.current}`);
+    // console.log('formData: ', formData);
 
     return (
-        <form ref={signupForm} action="" noValidate onSubmit={handleSubmit}>
+        <form ref={signupForm} action="" noValidate onKeyUp={handleKeyUp} onKeyDown={handleKeyDown}>
             <input id="user" type="text" data-error="Must have a user" validation-message="custom message here" required/>
             <input id="email" type="email" data-error="Check if email is correct" required/>
             <div className="wtf">wtf</div>
             <input id="pass" type="password" minLength={8} data-error="Password must be at least 8 characters long" required/>
             <input id="pass-confirm" type="password" data-error="Passwords must match" minLength={8} required/>
-            <input type="submit" value="send" />
+            <input type="submit" value="send" onClick={handleSubmit}/>
         </form>
     )
 }
