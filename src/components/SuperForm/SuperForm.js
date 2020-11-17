d9bc9a63389d7d2d5dc70e883ab7f41aa6e85252
Valentin Harapu <valentin.harapu@vspartners.us>
2020-11-17 08:59:09 +0200

    prettify

--- src/components/SuperForm/SuperForm.js
+++ src/components/SuperForm/SuperForm.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect, createRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import './SuperForm.scss';
 
 /*
@@ -13,20 +13,20 @@ TODO
 
 // TODO add event listeners to the form (using event delegation) to alter state when necessary, validate on blur etc.
 
-// use these attributes for validation 
-// console.log('valid: ', e.target[0].validity.valid); 
+// use these attributes for validation
+// console.log('valid: ', e.target[0].validity.valid);
 // console.log('value missing: ', e.target[0].validity.valueMissing);
 
 const ENTER = 13;
 
 const SuperForm = () => {
-    const [formData, setFormData] = useState({})
+    const [formData, setFormData] = useState({});
     const signupForm = useRef(null);
     const renderCount = useRef(0);
 
-    const createStateObjFromForm = (form) => {
+    const createStateObjFromForm = form => {
         let state = {};
-        new Array(...form.elements).forEach( ( input, idx, arr )=> {
+        new Array(...form.elements).forEach((input, idx, arr) => {
             if (input.type === 'submit') return;
             const inputState = {
                 index: idx,
@@ -34,55 +34,82 @@ const SuperForm = () => {
                 value: input.value,
                 touched: false,
                 error: input.dataset.error,
-                nextInput: arr[idx+1],
-            }
-            state = {...state, [input.id]: inputState };
+                nextInput: arr[idx + 1],
+            };
+            state = { ...state, [input.id]: inputState };
         });
         return state;
-    }
+    };
 
     const handleKeyDown = e => {
-        if(e.keyCode == 13) e.preventDefault();
-    }
+        if (e.keyCode === ENTER) e.preventDefault();
+    };
 
     const handleKeyUp = e => {
         e.preventDefault();
-        if (e.keyCode === ENTER && e.target.id ) return formData[e.target.id].nextInput.focus();
-        if (e.target.type === 'submit') return handleSubmit(); 
-        const formState = {...formData};
+        if (e.keyCode === ENTER && e.target.id)
+            return formData[e.target.id].nextInput.focus();
+        if (e.target.type === 'submit') return handleSubmit();
+        const formState = { ...formData };
         formState[e.target.id].value = e.target.value;
-        setFormData(formState);        
-    }
-    
+        setFormData(formState);
+    };
+
     useEffect(() => {
         if (signupForm.current) {
             const state = createStateObjFromForm(signupForm.current);
             setFormData(state);
         }
 
-        return () => {
-            
-        }
-    }, [])
+        return () => {};
+    }, []);
 
-    const handleSubmit = (e) => {
-        if (e) e.preventDefault();
+    const handleSubmit = e => {
+        e.preventDefault();
         console.log('SUBMITTED');
-    }
-    
+    };
+
     renderCount.current++;
     console.log(`render number ${renderCount.current}`);
     // console.log('formData: ', formData);
 
     return (
-        <form ref={signupForm} action="" noValidate onKeyUp={handleKeyUp} onKeyDown={handleKeyDown}>
-            <input id="user" type="text" data-error="Must have a user" validation-message="custom message here" required/>
-            <input id="email" type="email" data-error="Check if email is correct" required/>
+        <form
+            ref={signupForm}
+            noValidate
+            onKeyUp={handleKeyUp}
+            onKeyDown={handleKeyDown}
+        >
+            <input
+                id="user"
+                type="text"
+                data-error="Must have a user"
+                validation-message="custom message here"
+                required
+            />
+            <input
+                id="email"
+                type="email"
+                data-error="Check if email is correct"
+                required
+            />
             <div className="wtf">wtf</div>
-            <input id="pass" type="password" minLength={8} data-error="Password must be at least 8 characters long" required/>
-            <input id="pass-confirm" type="password" data-error="Passwords must match" minLength={8} required/>
-            <input type="submit" value="send" onClick={handleSubmit}/>
+            <input
+                id="pass"
+                type="password"
+                minLength={8}
+                data-error="Password must be at least 8 characters long"
+                required
+            />
+            <input
+                id="pass-confirm"
+                type="password"
+                data-error="Passwords must match"
+                minLength={8}
+                required
+            />
+            <input type="submit" value="send" onClick={handleSubmit} />
         </form>
-    )
-}
+    );
+};
 export default SuperForm;
